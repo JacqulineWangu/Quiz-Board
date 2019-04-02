c497a244978c99508709ed64e2d5749b50301425
jacquline <jacqulinewangu@gmail.com>
2019-04-02 12:17:02 +0300

    made changes in the index.html && js

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,9 @@ $(document).ready(function () {
         var question7 = parseInt($('input:radio[name=ans7] : checked').val("C"));
         var question8 = parseInt($('input:radio[name=ans8] : checked').val("B"));
         var question9 = parseInt($('input:radio[name=ans9] : checked').val("C"));
-        var question10 = parseInt($('input:radio[name=ans10] : checked').val("B"));
+        var question10 = parseInt($('input:radio[name=ans10] : checked').val());
         var result = add(question1, question2, question3, question4, question5, question6, question7, question8, question9, question10);
-    });
-});
+
 
 $("#result").text("YOUR SCORE: " + result + " POINTS OUT OF 10");
 
