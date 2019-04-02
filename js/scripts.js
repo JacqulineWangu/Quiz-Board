4d663e40b78e020c579cbf455ab9e38da8ae9493
jacquline <jacqulinewangu@gmail.com>
2019-04-02 08:22:55 +0300

    made changes to js

--- js/scripts.js
+++ js/scripts.js
@@ -19,33 +19,3 @@ $("#quiz1").submit(function (event){
  });
 });
 
-  // UI logic
-$(document).ready(function() {
-    //to hide our questions
-    $('.jsForm').hide();
-  
-    //to show the questions one by one
-    $('#quiz1').show();
-  
-    $('.jsForm #submit').click(function() {
-      current = $(this).parents('form:first').data('question');
-      next = $(this).parents('form:first').data('question') + 1;
-  
-      $('.jsForm').hide();
-      $('#quiz' + next + '').fadeToggle(1500);
-      process('' + current + '');
-      return false;
-    });
-  
-  });
-  
-  function process(n) {
-    var mySubmitted = $('input[name=ans' + n + ']:checked').val();
-    if (mySubmitted == value('a' + n + '')) {
-      score++;
-    }
-    if (n == total) {
-      $('#results').html('<h3>Your final score is: ' + score + ' out of 10</h3><a href="index.html">Take Quiz Again</a>');
-    }
-    return false;
-  }
