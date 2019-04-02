f0c52cb2b83844298caa9517b2b6bef19d211eb9
jacquline <jacqulinewangu@gmail.com>
2019-04-02 12:22:45 +0300

    made changes in the scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,15 @@ function add(question1, question2, question3, question4, question5, question6, q
 $(document).ready(function () {
     $("#quiz1").submit(function (event) {
         event.preventDefault();
-        var question1 = parseInt($('input:radio[name=ans1] : checked').val("D"));
-        var question2 = parseInt($('input:radio[name=ans2] : checked').val("D"));
-        var question3 = parseInt($('input:radio[name=ans3] : checked').val("C"));
-        var question4 = parseInt($('input:radio[name=ans4] : checked').val("D"));
-        var question5 = parseInt($('input:radio[name=ans5] : checked').val("D"));
-        var question6 = parseInt($('input:radio[name=ans6] : checked').val("C"));
-        var question7 = parseInt($('input:radio[name=ans7] : checked').val("C"));
-        var question8 = parseInt($('input:radio[name=ans8] : checked').val("B"));
-        var question9 = parseInt($('input:radio[name=ans9] : checked').val("C"));
+        var question1 = parseInt($('input:radio[name=ans1] : checked').val());
+        var question2 = parseInt($('input:radio[name=ans2] : checked').val());
+        var question3 = parseInt($('input:radio[name=ans3] : checked').val());
+        var question4 = parseInt($('input:radio[name=ans4] : checked').val());
+        var question5 = parseInt($('input:radio[name=ans5] : checked').val());
+        var question6 = parseInt($('input:radio[name=ans6] : checked').val());
+        var question7 = parseInt($('input:radio[name=ans7] : checked').val());
+        var question8 = parseInt($('input:radio[name=ans8] : checked').val());
+        var question9 = parseInt($('input:radio[name=ans9] : checked').val());
         var question10 = parseInt($('input:radio[name=ans10] : checked').val());
         var result = add(question1, question2, question3, question4, question5, question6, question7, question8, question9, question10);
 
