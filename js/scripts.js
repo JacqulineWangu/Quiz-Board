7851875bd137b4c8929a27594675c7ac7d7dd0dd
jacquline <jacqulinewangu@gmail.com>
2019-04-02 07:18:30 +0300

    added ans on js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
+function add(question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) {return question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10};
 
+$(document.readyState()  (parameter) event: any
+$("#quiz").submit(function (event){
+    event.preventDefault() ;
+    var question1 = parseInt ($('input:radio[name=ans1] : checked').val("D"));
+    var question2 = parseInt ($('input:radio[name=ans2] : checked').val("D"));
+    var question3 = parseInt ($('input:radio[name=ans3] : checked').val("C"));
+    var question4 = parseInt ($('input:radio[name=ans4] : checked').val("D"));
+    var question5 = parseInt ($('input:radio[name=ans5] : checked').val("D"));
+    var question6 = parseInt ($('input:radio[name=ans6] : checked').val("C"));
+    var question7 = parseInt ($('input:radio[name=ans7] : checked').val("C"));
+    var question8 = parseInt ($('input:radio[name=ans8] : checked').val("B"));
+    var question9 = parseInt ($('input:radio[name=ans9] : checked').val("C"));
+    var question10 = parseInt ($('input:radio[name=ans10] : checked').val("B"));
+    var result = add(question1, question2, question3, question4, question5, question6, question7, question8, question9, question10);
+}))
+
+  // UI logic
 $(document).ready(function() {
+    //to hide our questions
     $('.jsForm').hide();
   
+    //to show the questions one by one
     $('#quiz1').show();
   
     $('.jsForm #submit').click(function() {
@@ -16,22 +36,9 @@ $(document).ready(function() {
   
   });
   
-  var score = 0;
-  var total = 5;
-  var points = 1;
-  var highest = 'total + point';
-  
-  function init() {
-    sessionStorage.setItem('a1', 'd');
-    sessionStorage.setItem('a2', 'a');
-    sessionStorage.setItem('a3', 'd');
-    sessionStorage.setItem('a4', 'b');
-    sessionStorage.setItem('a5', 'c');
-  }
-  
   function process(n) {
-    var mySubmitted = $('input[name=quiz' + n + ']:checked').val();
-    if (mySubmitted == sessionStorage.getItem('a' + n + '')) {
+    var mySubmitted = $('input[name=ans' + n + ']:checked').val();
+    if (mySubmitted == value('a' + n + '')) {
       score++;
     }
     if (n == total) {
@@ -39,5 +46,3 @@ $(document).ready(function() {
     }
     return false;
   }
-  
-  window.addEventListener('load', init, false);
